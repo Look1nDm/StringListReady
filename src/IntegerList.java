1c87e18754953a66546f0446909e52dd5a887c89
dimka <dimka411841994@gmail.com>
2022-09-23 19:31:17 +0300

    ДЗ по написанию своего подобия ArrayList для Integer

---
+++ src/IntegerList.java
@@ -0,0 +1,197 @@
+import exceptions.IndexNotFoundException;
+import exceptions.ItemIsNullException;
+import exceptions.TestListOfBoundException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class IntegerList {
+    private final Integer[] list;
+    private static int size;
+
+    public IntegerList() {
+        this.list = generateRandomArray(100000);
+    }
+
+    public IntegerList(int s) {
+        list = new Integer[s];
+    }
+
+    public Integer[] generateRandomArray(int bound) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < bound; i++) {
+            Integer in = random.nextInt(100000)+100000;
+            list.add(in);
+            System.out.println(list.size());
+        }
+        return list.toArray(new Integer[0]);
+    }
+    private void validString(Integer item) {
+        if (item == null) {
+            throw new ItemIsNullException("Переданная строка ничего не содержит!");
+        }
+    }
+
+    private void validIndex(Integer index) {
+        if (index > size || index < 0) {
+            throw new IndexNotFoundException("Не найти такого индекса, Повелитель!");
+        }
+    }
+
+    private void validSize() {
+        if (size == list.length) {
+            throw new TestListOfBoundException("Список заполнен, мой господин!");
+        }
+    }
+    // 1 2 3 4 5 6 7 8 9
+    public Integer binarySearch(int number){//2
+        int count = list.length/2;//5
+        int[] array = new int[list.length];
+        while (number!=count) {
+            if (number > array.length / 2) {
+                System.arraycopy(list, list.length / 2, array, 0, list.length / 2);
+            }
+            if (number < array.length / 2) {
+                System.arraycopy(list, 0, array, 0, list.length / 2);
+            }
+        }
+        return count;
+    }
+    public void sort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+
+    public Integer add(Integer item) {
+        validSize();
+        validString(item);
+        return list[size++] = item;
+    }
+
+    public Integer add(int index, Integer item) {
+        validSize();
+        validString(item);
+        validIndex(index);
+        if (index == size) {
+            return list[size++] = item;
+        }
+        System.arraycopy(list, index, list, index+1, size - index);
+        return list[index] = item;
+    }
+
+    public Integer set(int index, Integer item) {
+        validIndex(index);
+        validString(item);
+        return list[index] = item;
+    }
+
+    public Integer remove(Integer item) {
+        int i = indexOf(item);
+        validString(item);
+        if(i==-1){
+            throw new IndexNotFoundException("Нет такого элемента, повелитель!");
+        }
+        if(i==size){
+            list[size--] = null;
+            return item;
+        }
+        System.arraycopy(list,i+1,list,i,size-i);
+        return item;
+    }
+
+    public Integer remove(int index) {
+        validIndex(index);
+        if(index==-1){
+            throw new IndexNotFoundException("Нет такого элемента, повелитель!");
+        }
+        Integer s = list[index];
+        if(index == size){
+            list[size--]=null;
+            return s;
+        }
+        System.arraycopy(list,index+1,list,index,size-index);
+        return s;
+    }
+    public boolean contains(Integer item) {
+        validIndex(item);
+
+        Integer [] array = toArray();
+        sort(list);
+        int min = 0;
+        int max = list.length - 1;
+        while (min <= max){
+            int mid = min+max/2;
+            if (item.equals(array[mid])){
+                return true;
+            }
+            if (item < array.length/2){
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        } return false;
+    }
+
+
+    public int indexOf(Integer item) {
+        for (int i = 0; i < size; i++) {
+            if (list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Integer item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (list[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Integer get(int index) {
+        validIndex(index);
+        return list[index];
+    }
+
+    public boolean equals(IntegerList otherList) {
+        if (this == otherList) return true;
+        if (otherList == null || getClass() != otherList.getClass()) return false;
+        IntegerList testList = otherList;
+        return size == testList.size && Arrays.equals(list, testList.list);
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public void clear() {
+        size = 0;
+    }
+
+
+    public Integer[] toArray() {
+        return Arrays.copyOf(list, size);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class Main {
+
+import java.util.Random;
+public class Main {
+    private static final Random random = new Random();
+    public static void main(String[] args) {
+//        double timeForBobbleSort = timeForBubbleSort(5);
+//        System.out.println("Время пузырьковой сортировки: "+timeForBobbleSort);
+//        double timeForInsperetionSort = timeForSortInsertion(5);
+//        System.out.println("Время вставочной сортировки: "+timeForInsperetionSort);
+//        double timeForSelectSort = timeForSortSelection(5);
+//        System.out.println("Время перебором: "+timeForSelectSort);
+    }
+    private static void swapElements(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+//    public static void sortSelection(int[] arr) {
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            int minElementIndex = i;
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[j] < arr[minElementIndex]) {
+//                    minElementIndex = j;
+//                }
+//            }
+//            swapElements(arr, i, minElementIndex);
+//        }
+//    }
+//    public static void sortInsertion(int[] arr) {
+//        for (int i = 1; i < arr.length; i++) {
+//            int temp = arr[i];
+//            int j = i;
+//            while (j > 0 && arr[j - 1] >= temp) {
+//                arr[j] = arr[j - 1];
+//                j--;
+//            }
+//            arr[j] = temp;
+//        }
+//    }
+//    public static void sortBubble(int[] arr) {
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = 0; j < arr.length - 1 - i; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    swapElements(arr, j, j + 1);
+//                }
+//            }
+//        }
+//    }
+//    private static double timeForBubbleSort(int iter){
+//        double sum = 0;
+//        for (int i = 0; i<iter;i++){
+//            int[] array = generateArrays(100000);
+//            long start = System.currentTimeMillis();
+//            sortBubble(array);
+//            long end = System.currentTimeMillis() - start;
+//            sum+=end;
+//        }
+//        return sum/iter;
+//    }
+//    private static double timeForSortInsertion(int iter){
+//        double sum = 0;
+//        for (int i = 0; i<iter;i++){
+//            int[] array = generateArrays(100000);
+//            long start = System.currentTimeMillis();
+//            sortInsertion(array);
+//            long end = System.currentTimeMillis() - start;
+//            sum+=end;
+//        }
+//        return sum/iter;
+//    }
+//    private static double timeForSortSelection(int iter){
+//        double sum = 0;
+//        for (int i = 0; i<iter;i++){
+//            int[] array = generateArrays(100000);
+//            long start = System.currentTimeMillis();
+//            sortSelection(array);
+//            long end = System.currentTimeMillis() - start;
+//            sum+=end;
+//        }
+//        return sum/iter;
+//    }
+    private static int[] generateArrays(int bound){
+        int[] array = new int[bound];
+            for (int i =0; i < bound;i++){
+                array[i] = random.nextInt(-100,100);
+            }
+            return array;
+    }
 }
